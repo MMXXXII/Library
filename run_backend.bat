b0085c299bfa038c6cef4303b35c38021648872b
MMXXXII <perfilev621@gmail.com>
2026-09-18 16:13:15 +0800

    fix

--- run_backend.bat
+++ run_backend.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d C:\deploy\library-app
-python manage.py runserver 0.0.0.0:8000 > backend.log 2>&1
+pythonw manage.py runserver 0.0.0.0:8000 > backend.log 2>&1
--- run_frontend.bat
+++ run_frontend.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d C:\deploy\library-app\frontend
-python -m http.server 4173 > ..\frontend.log 2>&1
+pythonw -m http.server 4173 > ..\frontend.log 2>&1
